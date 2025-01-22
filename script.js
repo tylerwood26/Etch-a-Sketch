065491eabba972bc2c09434a17363bf22dde1710
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-01-22 13:20:53 -0500

    Check userinput and clears grid before making new grid

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function createGrid(number) {
         rowSquare.textContent = "";
         container.appendChild(rowSquare);
 
-        for (let j = 0; j < number - 1; j++) {
+        for (let j = 0; j < number; j++) {
             const colSquare = document.createElement("div");
             colSquare.classList.add("col");
             colSquare.classList.add("hover");
@@ -14,16 +14,39 @@ function createGrid(number) {
             rowSquare.appendChild(colSquare);
         }
     }
+
 }
 
 const btn = document.querySelector(".grid-btn");
-btn.addEventListener("click", () => {
-    let userInput = prompt("How big would you like the grid?")
-    createGrid(userInput);
+btn.addEventListener("click", btnPressed);
 
-});
+function checkUserInput(input) {
+    if (isNaN(input)) {
+        btnPressed();
+    }
+    if (input < 1 || input > 100) {
+        btnPressed();
+    }
+}
 
-// make sure userInput is number
-// max 100
+function clearGrid() {
+    const nodeList = document.querySelectorAll(".row");
+    if (nodeList.length === 0) {
+        return
+    } else {
+        for (let i = 0; i < nodeList.length; i++) {
+            nodeList[i].remove();
+        }
+    }
+}
+
+function btnPressed() {
+    clearGrid();
+    let userInput = prompt("How big would you like the grid? (must be positive number below 100)");
+    checkUserInput(userInput);
+    let nodeList = createGrid(userInput);
+}
+
+
 // if buttons pressed after grid made
     // delete current grid and make new one
