c12bac7382ff1d110f86840b3e56749777030b1d
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-01-22 12:21:26 -0500

    User input creates grid

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-function createGrid() {
+function createGrid(number) {
     const container = document.querySelector(".container");
-    for (let i = 0; i < 16; i++) {
-        const div = document.createElement("div");
-        container.appendChild(div);
+    for (let i = 0; i < number; i++) {
+        const rowSquare = document.createElement("div");
+        rowSquare.classList.add("row");
+        rowSquare.textContent = "";
+        container.appendChild(rowSquare);
+
+        for (let j = 0; j < number - 1; j++) {
+            const colSquare = document.createElement("div");
+            colSquare.classList.add("col");
+            colSquare.classList.add("hover");
+            colSquare.textContent = "";
+            rowSquare.appendChild(colSquare);
+        }
     }
 }
 
-createGrid();
+const btn = document.querySelector(".grid-btn");
+btn.addEventListener("click", () => {
+    let userInput = prompt("How big would you like the grid?")
+    createGrid(userInput);
+
+});
+
+// make sure userInput is number
+// max 100
+// if buttons pressed after grid made
+    // delete current grid and make new one
