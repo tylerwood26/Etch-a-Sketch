50354f49d3f56f9768452b3d20eaf1d0334e4a0d
tylerwood26 <24847346+tylerwood26@users.noreply.github.com>
2025-01-22 08:47:34 -0500

    Create 16 divs

---
+++ script.js
@@ -0,0 +1,9 @@
+function createGrid() {
+    const container = document.querySelector(".container");
+    for (let i = 0; i < 16; i++) {
+        const div = document.createElement("div");
+        container.appendChild(div);
+    }
+}
+
+createGrid();
